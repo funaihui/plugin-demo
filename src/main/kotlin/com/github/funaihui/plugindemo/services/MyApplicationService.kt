db9271bab53b589dee40adfba1a8a5396aac79ae
GitHub Action <action@github.com>
2022-12-05 08:38:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/funaihui/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/funaihui/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.funaihui.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.funaihui.plugindemo.MyBundle
 
 class MyApplicationService {
 
